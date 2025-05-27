810bce9eddae470c9bccff778259da2674f4e6f9
Momonga-OP <latifdofus@gmail.com>
2025-05-27 03:47:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from googletrans import Translator  # Import Translator
 import datetime
 from math import ceil
 
-
-# Logging setu
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
